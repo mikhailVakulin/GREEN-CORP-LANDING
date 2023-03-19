74390e25603535d4db175994c8f125ffc99b49ff
mikhailVakulin <mikhail.vakulin@gmail.com>
2023-03-20 01:23:00 +0300

    create new elements for our project

--- js/animation.js
+++ js/animation.js
@@ -19,3 +19,25 @@ function initIncreaseNumberAnimation() {
 }
 initIncreaseNumberAnimation();
 console.log(initIncreaseNumberAnimation());
+
+document
+  .querySelector("#budget")
+  .addEventListener("change", function handleSelectChange(event) {
+    if (event.target.value === "other") {
+      const formContainer = document.createElement("div");
+      formContainer.classList.add("form__group");
+      formContainer.classList.add("form__other-input");
+      const input = document.createElement("input");
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text";
+      formContainer.appendChild(input);
+      document
+        .querySelector("form")
+        .insertBefore(formContainer, document.querySelector(".form__submit"));
+    }
+    const otherInput = document.querySelector(".form__other-input");
+
+    if (event.target.value !== "other" && Boolean(otherInput)) {
+      document.querySelector("form").removeChild(otherInput);
+    }
+  });
